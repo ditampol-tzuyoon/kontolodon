be0f2486a3f5b50a2591cd726e7e93f7cedce15e
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-12-21 01:41:26 +0700

    Update scikon.lua

--- scikon.lua
+++ scikon.lua
@@ -1,3 +1,4 @@
 ListPembeli = {
-    {iddc = "422670528483033088", nick = "OdStillAlive"}
+    {iddc = "422670528483033088", nick = "OdStillAlive"},
+    {iddc = "422670528483033088", nick = "OdWasHerez"}
 }
