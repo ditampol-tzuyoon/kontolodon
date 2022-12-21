ee6eb924e5f099b8b8a7886cb676a08ac3e14097
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2022-12-21 19:21:00 +0700

    Update rotkon.lua

--- rotkon.lua
+++ rotkon.lua
@@ -300,7 +300,7 @@ ListPembeli = {
     {iddc = "743301080451907676", nick = "BUILTDIFFZZZ"},
     {iddc = "709380517006409800", nick = "BananaCacah"},
     {iddc = "419455338295721984", nick = "BeUse"},
-    {iddc = "783170691070558229", nick = "gaysurg1270"},
+    {iddc = "783170691070558229", nick = "rtmentos50"},
     {iddc = "935581740280463430", nick = "goktug1000"},
     {iddc = "801687196725805067", nick = "Fawdy"},
     {iddc = "443788266626351114", nick = "iRiyuuun"},
