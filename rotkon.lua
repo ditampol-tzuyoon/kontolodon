bac4511e6fbe29d9cfab993779c000268bc21821
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-01-28 08:00:45 +0700

    Update rotkon.lua

--- rotkon.lua
+++ rotkon.lua
@@ -333,7 +333,7 @@ ListPembeli = {
     {iddc = "937693157133516900", nick = "tkgberas2"},
     {iddc = "666202668896616448", nick = "LJZDJNT"},
     {iddc = "282378216356708352", nick = "ENREMITEGL"},
-    {iddc = "642020164761550858", nick = "AZxiaoGT"},
+    {iddc = "642020164761550858", nick = "Nekopunch11"},
     {iddc = "807609578535125032", nick = "AstroxFord"},
     {iddc = "862706780068905010", nick = "veldpucek01"},
     {iddc = "705040932289904711", nick = "AserA1"},
