71bd0485a59014047e95a5105099bc2911f1499b
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-02-10 18:52:58 +0700

    Update rotkon.lua

--- rotkon.lua
+++ rotkon.lua
@@ -242,7 +242,7 @@ ListPembeli = {
     {iddc = "949474781290463294", nick = "KangCMDIdann"},
     {iddc = "818358695737032725", nick = "SALLKUY28"},
     {iddc = "412931519577980940", nick = "ownShanum"},
-    {iddc = "974162242436210698", nick = "PESANG"},
+    {iddc = "974162242436210698", nick = "PESANGS"},
     {iddc = "421775009204273154", nick = "hitambabu199"},
     {iddc = "946739732195442748", nick = "TUGIYO"},
     {iddc = "811774818524332082", nick = "ZZOOGABEQ"},
