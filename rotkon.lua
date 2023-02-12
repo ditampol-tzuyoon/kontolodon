c23d3e24418f919d7ec4572385fc6918dc84a9c7
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-02-12 21:04:22 +0700

    Update rotkon.lua

--- rotkon.lua
+++ rotkon.lua
@@ -515,5 +515,6 @@ ListPembeli = {
     {iddc = "911291550728855593", nick = "XINxTAKA1"},
     {iddc = "445845706293641218", nick = "BadInternets"},
     {iddc = "1031784367439220736", nick = "Jaju31"},
-    {iddc = "838439967176785961", nick = "eluzz"}
+    {iddc = "838439967176785961", nick = "eluzz"},
+    {iddc = "513285129280552990", nick = "BrustFalls1234"}
 }
