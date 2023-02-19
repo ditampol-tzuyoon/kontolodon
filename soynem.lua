1c41c570786889a6c139c1d7135410561cec8534
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-02-20 01:08:35 +0700

    Create soynem.lua

---
+++ soynem.lua
@@ -0,0 +1,8 @@
+ListPembeli = {
+    {iddc = "422670528483033088", nick = "DearBil"},
+    {iddc = "1019129204060717129", nick = "OzBjorn"},
+    {iddc = "946047195965177946", nick = "HISSP"},
+    {iddc = "417193943457988608", nick = "CoilZwo1"},
+    {iddc = "618793315931914269", nick = "N1XI"},
+    {iddc = "884499020817506314", nick = "pdrnin"}
+}
