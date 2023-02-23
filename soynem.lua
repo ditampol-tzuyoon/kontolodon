677fb79695cf71f67e3b8bd4981a0f04858ef877
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-02-23 10:53:03 +0700

    Update soynem.lua

--- soynem.lua
+++ soynem.lua
@@ -1,5 +1,5 @@
 ListPembeli = {
-    {iddc = "422670528483033088", nick = "DearBil"},
+    {iddc = "422670528483033088", nick = "DearBiil"},
     {iddc = "1019129204060717129", nick = "OzBjorn"},
     {iddc = "946047195965177946", nick = "HISSP"},
     {iddc = "417193943457988608", nick = "CoilZwo1"},
