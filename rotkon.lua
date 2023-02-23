d2006cd344e32c76c10d3d897a04baf67c38159a
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-02-24 01:13:36 +0700

    Update rotkon.lua

--- rotkon.lua
+++ rotkon.lua
@@ -219,7 +219,7 @@ ListPembeli = {
     {iddc = "942277519376252959", nick = "SURGPUNG200"},
     {iddc = "689653676863914009", nick = "j3Brgu0S"},
     {iddc = "766997063816183839", nick = "YCQEAJI"},
-    {iddc = "544814666514169888", nick = "Suksesbul"},
+    {iddc = "544814666514169888", nick = "Bisabul"},
     {iddc = "622674180588175370", nick = "irtolsurg10"},
     {iddc = "988420746630004776", nick = "CalvinMakaminang"},
     {iddc = "559388812992708609", nick = "Anatomies"},
