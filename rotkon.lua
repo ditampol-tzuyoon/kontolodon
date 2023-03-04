21bf01c52b856c4738d733c7a047bc457803bb1f
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-03-04 22:42:13 +0700

    Update rotkon.lua

--- rotkon.lua
+++ rotkon.lua
@@ -566,5 +566,6 @@ ListPembeli = {
     {iddc = "910769414780108820", nick = "PinkyLoli"},
     {iddc = "112716245035409408", nick = "JodyBGL"},
     {iddc = "403091708885073921", nick = "amaterasugan"},
-    {iddc = "460281514543022092", nick = "RIOWILKANMEMEQ"}
+    {iddc = "460281514543022092", nick = "RIOWILKANMEMEQ"},
+    {iddc = "698118656735051776", nick = "LenMouse42337"}
 }
