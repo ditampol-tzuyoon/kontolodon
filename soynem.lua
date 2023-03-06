84884123a60ddaaec2e0d3fac716f64a1fc5ecf2
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-03-07 00:01:28 +0700

    Update soynem.lua

--- soynem.lua
+++ soynem.lua
@@ -4,7 +4,7 @@ ListPembeli = {
     {iddc = "946047195965177946", nick = "HISSP"},
     {iddc = "417193943457988608", nick = "CoilZwo1"},
     {iddc = "618793315931914269", nick = "Kazuma55"},
-    {iddc = "884499020817506314", nick = "pdrnin"},
+    {iddc = "884499020817506314", nick = "pfrnin"},
     {iddc = "390747370968580100", nick = "santy04"},
     {iddc = "444489519861858304", nick = "lefinadd"}
 }
