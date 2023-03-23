41ccb305351d50995701299136328feb8ecd1536
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-03-23 20:07:23 +0700

    Update rotkon.lua

--- rotkon.lua
+++ rotkon.lua
@@ -447,7 +447,7 @@ ListPembeli = {
     {iddc = "536570127433465857", nick = "KYYBASS"},
     {iddc = "547032936453767169", nick = "dranest27"},
     {iddc = "1023942510004932658", nick = "rizkyxop"},
-    {iddc = "513720454481969153", nick = "Cjeis"},
+    {iddc = "513720454481969153", nick = "DPutt"},
     {iddc = "753201963948048394", nick = "AmKairi"},
     {iddc = "893804482561982495", nick = "Putrarian5"},
     {iddc = "463619190004252673", nick = "Saliaaa"},
